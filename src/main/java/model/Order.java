da5edddda2372562a34885840409f85bce849198
Andrus A <75877044+andrusand@users.noreply.github.com>
2020-12-20 11:47:20 +0200

    Update Order.java

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,4 +1,36 @@
 package model;
 
+import javax.persistence.*;
+
+@Entity
+@Table (name = "order")
+
 public class Order {
+
+@Id
+@GeneratedValue (strategy = GenerationType.IDENTITY)
+
+@ManyToOne
+@JoinColumn(name = "productId")
+private Product product;
+
+@ManyToOne
+@JoinColumn(name = "customerId")
+private Customer customer;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
