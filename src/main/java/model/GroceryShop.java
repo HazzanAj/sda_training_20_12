c9db7a16a1ed62af73ba8ea7dffe2d6cbe4c2bc2
mmainulhossain <75877320+mmainulhossain@users.noreply.github.com>
2020-12-20 11:26:20 +0200

    Update shop

--- src/main/java/model/GroceryShop.java
+++ src/main/java/model/GroceryShop.java
@@ -1,4 +1,55 @@
 package model;
 
-public class GroceryShop {
-}
+import javax.persistence.*;
+
+
+
+
+    @Entity
+    @Table(name = "GroceryShop")
+    public class GroceryShop {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "shopId", insertable = false, nullable = false)
+        private Integer shopId;
+        private String shopName;
+        private String address;
+        private String location;
+
+        public Integer getShopId() {
+            return shopId;
+        }
+
+        public void setShopId(Integer shopId) {
+            this.shopId = shopId;
+        }
+
+        public String getShopName() {
+            return shopName;
+        }
+
+        public void setShopName(String shopName) {
+            this.shopName = shopName;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+    }
+
+
+
+
+
