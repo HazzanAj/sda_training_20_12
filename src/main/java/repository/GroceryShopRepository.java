100a8239adec1ec150f407a107b3e10340a0453c
mmainulhossain <75877320+mmainulhossain@users.noreply.github.com>
2020-12-20 11:47:14 +0200

    Update GroceryShopRepository.java

---
+++ src/main/java/repository/GroceryShopRepository.java
@@ -0,0 +1,58 @@
+package repository;
+
+import model.GroceryShop;
+import util.DbUtil;
+import java.util.*;
+
+import javax.persistence.EntityManager;
+
+public class GroceryShopRepository {
+
+    private EntityManager rm;
+
+    public GroceryShopRepository() {
+        rm = DbUtil.getEntityManager();
+    }
+
+    public void saveShop(GroceryShop groceryShop) {
+        try {
+            rm.getTransaction().begin();
+            rm.persist(groceryShop);
+            rm.getTransaction().commit();
+        } catch (Exception ex) {
+            rm.getTransaction().rollback();
+        }
+    }
+
+    public void updateShop(GroceryShop groceryShop) {
+        try {
+            rm.getTransaction().begin();
+            rm.merge(groceryShop);
+            rm.getTransaction().commit();
+        } catch (Exception ex) {
+            rm.getTransaction().rollback();
+        }
+    }
+
+
+
+    public void deleteShop(GroceryShop groceryShop) {
+        try {
+            rm.getTransaction().begin();
+            rm.remove(groceryShop);
+            rm.getTransaction().commit();
+        } catch (Exception ex) {
+            rm.getTransaction().rollback();
+        }
+    }
+
+    public List<GroceryShop> listAllShops() {
+        String sql = "from GroceryShop rm";
+        return rm.createQuery(sql).getResultList();
+    }
+
+
+}
+
+
+
