2c39c38e20ee70b0075284646d2740f4f30a6c50
Zahidul Haque <75877216+zahidulhq@users.noreply.github.com>
2020-12-20 10:40:07 +0200

    Zahidul

---
+++ src/com/company/product.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class product {
+    private int productID;
+    private int shopID;
+    private String productName;
+    private int productPrice;
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public int getShopID() {
+        return shopID;
+    }
+
+    public void setShopID(int shopID) {
+        this.shopID = shopID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+}
