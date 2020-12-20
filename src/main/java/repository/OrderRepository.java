8724eea2bc8de71d2c7af11d139a70f805d2acb1
Andrus A <75877044+andrusand@users.noreply.github.com>
2020-12-20 12:00:56 +0200

    Update OrderRepository.java

--- src/main/java/repository/OrderRepository.java
+++ src/main/java/repository/OrderRepository.java
@@ -1,4 +1,57 @@
 package repository;
 
+import model.Order;
+import util.DbUtil;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class OrderRepository {
+
+    private EntityManager em;
+
+    public OrderRepository() {
+        em = DbUtil.getEntityManager();
+    }
+
+    public void save (Order order) {
+        try {
+            em.getTransaction().begin();
+            em.persist(order);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            em.getTransaction().rollback();
+        }
+    }
+
+    public void update (Order order) {
+        try {
+            em.getTransaction().begin();
+            em.merge(order);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            em.getTransaction().rollback();
+        }
+    }
+
+    public void delete (Order order) {
+        try {
+            em.getTransaction().begin();
+            em.remove(em.merge(order));
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            em.getTransaction().rollback();
+        }
+    }
+
+    public List<Order> listAll() {
+        String sql ="FROM Order o";
+        return em.createQuery(sql).getResultList();
+    }
+
+    public Order findById(int id) {
+        String sql = "FROM Client c WHERE c.clientId = :cId";
+        return em.createQuery(sql, Order.class).setParameter("cId", id).getSingleResult();
+    }
 }
+
